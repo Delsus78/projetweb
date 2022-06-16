bdea2fc90bb6ace4a46532f5e7c0d57e7fc87595
Delsus78 <delsustheugm@gmail.com>
2022-06-17 01:48:36 +0200

    script sql omg

--- sql-script.sql
+++ sql-script.sql
@@ -1,51 +1,85 @@
-DROP DATABASE tickettoc;
-CREATE DATABASE IF NOT EXISTS tickettoc;
+DROP DATABASE tickettac;
+CREATE DATABASE IF NOT EXISTS tickettac;
 
-USE tickettoc;
+USE tickettac;
 
-CREATE TABLE IF NOT EXISTS Projet (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100),
-    nomEntreprise VARCHAR(100)
-);
+CREATE TABLE tickettac.developpeur (
+  id INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(255) DEFAULT NULL,
+  prenom VARCHAR(255) DEFAULT NULL,
+  profilPicture VARCHAR(255) DEFAULT NULL,
+  email VARCHAR(50) DEFAULT NULL,
+  noisettes INT DEFAULT 0,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Client (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100),
-    prenom VARCHAR(100),
-    nomEntreprise VARCHAR(100)
-);
+CREATE TABLE tickettac.client (
+  id INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(50) DEFAULT NULL,
+  prenom VARCHAR(255) DEFAULT NULL,
+  entreprise VARCHAR(255) DEFAULT NULL,
+  email VARCHAR(50) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Ticket (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100),
-    nomEntreprise VARCHAR(100),
-    importance ENUM('HIGHT','MODERATE','LOW'),
-    etatAvancement ENUM('TODO','INPROGRESS','DONE'),
-    description TEXT,
-    date DATE,
-    idProjet INTEGER,
-    idClient INTEGER,
-    FOREIGN KEY (idProjet) REFERENCES Projet(id),
-    FOREIGN KEY (idClient) REFERENCES Client(id)
-);
+CREATE TABLE tickettac.projet (
+  id INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Developpeur (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100),
-    prenom VARCHAR(100),
-    noisettes INTEGER,
-    profilePicture text,
-    idTicket INTEGER,
-    FOREIGN KEY (idTicket) REFERENCES Ticket(id)
-);
+CREATE TABLE tickettac.rapporteur (
+  id INT NOT NULL AUTO_INCREMENT,
+  nom VARCHAR(255) DEFAULT NULL,
+  prenom VARCHAR(255) DEFAULT NULL,
+  email VARCHAR(50) DEFAULT NULL,
+  profilPicture VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 2,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci;
 
+CREATE TABLE tickettac.ticket (
+  id INT NOT NULL AUTO_INCREMENT,
+  idDev INT DEFAULT NULL,
+  idRapporteur INT NOT NULL,
+  nom VARCHAR(255) DEFAULT NULL,
+  date DATETIME DEFAULT NULL,
+  etatAvancement ENUM('A_FAIRE','EN_COURS','FINI') DEFAULT NULL,
+  importance ENUM('HIGH','MEDIUM','LOW') DEFAULT NULL,
+  description TEXT DEFAULT NULL,
+  idProjet INT NOT NULL,
+  idClient INT NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE = INNODB,
+AUTO_INCREMENT = 2,
+CHARACTER SET utf8mb4,
+COLLATE utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Rapporteur (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(100),
-    prenom VARCHAR(100),
-    profilePicture text,
-    idTicket INTEGER,
-    FOREIGN KEY (idTicket) REFERENCES Ticket(id)
-);
+ALTER TABLE tickettac.ticket
+  ADD CONSTRAINT FK_ticket_idClient FOREIGN KEY (idClient)
+    REFERENCES tickettac.client(id);
+
+ALTER TABLE tickettac.ticket
+  ADD CONSTRAINT FK_ticket_idDev FOREIGN KEY (idDev)
+    REFERENCES tickettac.developpeur(id);
+
+ALTER TABLE tickettac.ticket
+  ADD CONSTRAINT FK_ticket_idProjet FOREIGN KEY (idProjet)
+    REFERENCES tickettac.projet(id);
+
+ALTER TABLE tickettac.ticket
+  ADD CONSTRAINT FK_ticket_idRapporteur FOREIGN KEY (idRapporteur)
+    REFERENCES tickettac.rapporteur(id);
